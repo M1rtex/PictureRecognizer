3a9943aa99624baff0917b114f95f439ac1e6ab9
M1rtex <matoladanil@gmail.com>
2022-02-06 20:05:06 +0500

    Update 0.1: - bugs fixed - added everything you need to deploy the project

--- main.py
+++ main.py
@@ -16,13 +16,9 @@ def text_recognition(file_path):
     positions = info[0][0]
     start_x, fin_x, start_y, fin_y = positions[0][0], positions[1][0], positions[0][1], positions[2][1]
     img = img[start_y:fin_y, start_x:fin_x]
-    # cv2.imshow("res", img)
-    # cv2.waitKey(0)
+    cv2.imshow("res", img)
+    cv2.waitKey(0)
     result = pytesseract.image_to_string(img, config=custom_config).strip()
-    if result.split()[1][2] != '.' or result.split()[1][2] != ',':
-        result = result.split()[0] + " " + result.split()[1][:2] + '.' + result.split()[1][2:]
-    else:
-        result = result
     return result
 
 def telegram_bot(token):
@@ -32,7 +28,7 @@ def telegram_bot(token):
     def start_message(message):
         bot.send_message(message.chat.id, "Привет, я бот по распознаванию цифр с картинок.\nПришли мне картинку и я всё распознаю)")
 
-    @bot.message_handler(content_types=['document', 'text'])
+    @bot.message_handler(content_types=['document', 'text', 'photo'])
     def send_message(message):
         try:
             chat_id = message.chat.id
@@ -44,12 +40,12 @@ def telegram_bot(token):
             bot.reply_to(message, "Принял, понял, передал")
             data = text_recognition(src)
             with open(src, 'rb') as ph:
-                bot.send_document(message.chat.id, ph)
-                bot.send_message(message.chat.id, f"Всё готово! Вот данные:\n{data}")
+                # bot.send_document(message.chat.id, ph)
+                bot.reply_to(message, f"Всё готово! Вот данные:\n{data}")
             for file in glob('./img/*'):
                 remove(file)
         except Exception as e:
-            bot.reply_to(message, f'Ой ошибка:\n{e}   \nЯ не гугл, я не понимаю ((((((')
+            bot.reply_to(message, f'Ой ошибка:\n{e}\nЯ не гугл, я не понимаю ((((((')
 
     bot.polling()
 
