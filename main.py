7417a1af475cb72e5a45b3eedc0e54fd99708b8d
M1rtex <matoladanil@gmail.com>
2022-02-07 22:17:22 +0500

    final fix.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 custom_config = r"--oem 3 --psm 6"
 TOKEN = '5297439905:AAEmYVKrpWyhh_g8E5d0Upg-RVDQtOlhesg'
 
+
 def text_recognition(file_path):
     img = cv2.imread(file_path)
     reader = easyocr.Reader(["en"])
@@ -16,10 +17,11 @@ def text_recognition(file_path):
     positions = info[0][0]
     start_x, fin_x, start_y, fin_y = positions[0][0], positions[1][0], positions[0][1], positions[2][1]
     img = img[start_y:fin_y, start_x:fin_x]
-    cv2.imshow("res", img)
-    cv2.waitKey(0)
+    # cv2.imshow("res", img) # cropped preview
+    # cv2.waitKey(0) # cropped preview
     result = pytesseract.image_to_string(img, config=custom_config).strip()
     return result
+
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
@@ -28,7 +30,7 @@ def telegram_bot(token):
     def start_message(message):
         bot.send_message(message.chat.id, "Привет, я бот по распознаванию цифр с картинок.\nПришли мне картинку и я всё распознаю)")
 
-    @bot.message_handler(content_types=['document', 'text', 'photo'])
+    @bot.message_handler(content_types=['document', 'photo'])
     def send_message(message):
         try:
             chat_id = message.chat.id
@@ -46,6 +48,14 @@ def telegram_bot(token):
                 remove(file)
         except Exception as e:
             bot.reply_to(message, f'Ой ошибка:\n{e}\nЯ не гугл, я не понимаю ((((((')
+
+    @bot.message_handler(content_types=['text'])
+    def reply_to_text(message):
+        if message.text.lower() == "привет":
+            start_message(message)
+        else:
+            bot.send_message(message.chat.id,
+                             "Я работаю по принципу: картинка ---> результат.\nОтправь мне картинку и начнём работу!")
 
     bot.polling()
 
